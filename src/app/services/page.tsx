26782e95ebf3e928d753dcc9940579522fffc231
Bishal Mukherjee <bishalmukherjee448@gmail.com>
2025-02-16 01:26:44 +0530

    refactor: simplify Services page and move components

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,43 +1,9 @@
 "use client";
 
 import React from "react";
-// import { servicesData } from "@/components/modules/Services/servicesData";
-// import { ServiceTile } from "@/components/modules/Services/SerivceTile";
-// import { IdeaSection } from "@/components/modules/Services/IdeaSection";
-// import { motion, useInView } from "framer-motion";
+import { ServicesContents as Contents } from "@/components/modules/Services/Contents";
 
-// const variants = {
-//   hidden: { opacity: 0, y: 50 },
-//   visible: { opacity: 1, y: 0 },
-// };
-
-// const Contents = () => {
-//   const ref = useRef(null);
-//   const isInView = useInView(ref, { once: true });
-
-//   return (
-//     <div className="flex flex-col items-center justify-start py-24 bg-gradient-to-b from-slate-50 to-slate-100">
-//       <div ref={ref} className="space-y-8">
-//         {servicesData.map((service, index) => (
-//           <motion.div
-//             key={service.title}
-//             variants={variants}
-//             initial="hidden"
-//             animate={isInView ? "visible" : "hidden"}
-//             transition={{ duration: 0.5, delay: index * 0.2 }}
-//           >
-//             <ServiceTile isReversed={index % 2 === 0} {...service} />
-//           </motion.div>
-//         ))}
-//       </div>
-//       <div className="px-4 w-full flex justify-center">
-//         <IdeaSection />
-//       </div>
-//     </div>
-//   );
-// };
-
-const Srevices = () => {
+export const Services = () => {
   return (
     <div className="w-screen">
       <div className="flex flex-col justify-start items-center py-24 gap-4">
@@ -51,9 +17,7 @@ const Srevices = () => {
         </p>
       </div>
 
-      {/* <Contents /> */}
+      <Contents />
     </div>
   );
 };
-
-export default Srevices;
